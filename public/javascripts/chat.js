5e3155671e98c6728af5749d4f323c980279d055
MeghanaVasist <meghanavasist23@gmail.com>
2018-04-14 12:13:14 +0000

    Front end commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,23 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+    },
+        {
+            'sender': 'BOT',
+            'text': 'Hello, what can I do for you?'
+    },
+        {
+            'sender': 'USER',
+            'text': 'Help me find a nice place to eat'
+    },
+        {
+            'sender': 'BOT',
+            'text': 'Tell me where'
+    }
 
+	];
 });
