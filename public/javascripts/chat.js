b224d88d867a407cbdefa4cfb8b5af72bef3e744
MeghanaVasist <meghanavasist23@gmail.com>
2018-04-15 11:49:22 +0000

    Final commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,24 +6,27 @@ app.config(function ($mdThemingProvider) {
         .accentPalette('green');
 });
 
-app.controller('chatController', function ($scope) {
-    $scope.messages = [
-        {
-            'sender': 'USER',
-            'text': 'Hello'
-    },
-        {
-            'sender': 'BOT',
-            'text': 'Hello, what can I do for you?'
-    },
-        {
-            'sender': 'USER',
-            'text': 'Help me find a nice place to eat'
-    },
-        {
-            'sender': 'BOT',
-            'text': 'Tell me where'
-    }
+app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [];
+    $scope.trust = $sce.trustAsHtml;
 
-	];
+    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+
+    };
+
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = '';
+    };
+
+
+
 });
